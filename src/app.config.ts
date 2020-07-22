25b711d5bba3ca309ec726801d98b9e9c337dbd7
binbin <bin@seeapp.com>
2020-07-22 17:11:31 +0800

    demo init

---
+++ src/app.config.ts
@@ -0,0 +1,13 @@
+export default {
+  pages: [
+    'pages/index/index',
+    'pages/onepage/index',
+    'pages/twopage/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
---
+++ src/pages/twopage/index.ts
@@ -0,0 +1,7 @@
+import * as L from 'lodash';
+Component({
+
+  onLoad() {
+    L.map([1,2,3,], item => true)
+  }
+})
